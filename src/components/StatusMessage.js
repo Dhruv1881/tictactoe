5ac4a9c9a798b87502bc3007f40f5327bed0a4a1
Dhruv1881 <dhruvsharma1881@gmail.com>
2021-06-25 01:18:37 +0530

    displayed draw message with conditional rendering

---
+++ src/components/StatusMessage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const StatusMessage = ({ winner, current }) => {
+  const noMovesLeft = current.board.every(el => el !== null);
+  return (
+    <h1>
+      {winner && `Winner is ${winner}`}
+      {!winner &&
+        !noMovesLeft &&
+        `Next player is ${current.isXNext ? 'X' : 'O'}`}
+      {!winner && noMovesLeft && `X and O tied`}
+    </h1>
+  );
+};
+
+export default StatusMessage;
